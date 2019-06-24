cfffaa2c70c4a0bb48524f980895848b576e83fe
JGadi <johnpgadi@gmail.com>
2019-06-24 15:46:04 -0700

    modified datasources.production.js

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,20 +1,12 @@
-
-// module.exports = {
-//   "db": {
-//     "name": "db",
-//     "connector": "memory"
-//   },
-//   "MongoDB": {
-//     "url": "mongodb://localhost:27017",
-//     "name": "MongoDB",
-//     "connector": "mongodb"
-//   }
-// }
-
 module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
   "MongoDB": {
+    "url": "mongodb://localhost:27017",
     "name": "MongoDB",
-    "connector": "mongodb",
-    "url": process.env.MONGODB_URI
+    "connector": "mongodb"
   }
 }
+
