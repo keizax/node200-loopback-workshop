3a9f1faa63945d42f18b1cd9f62620637cec078f
JGadi <johnpgadi@gmail.com>
2019-06-24 15:22:38 -0700

    added circleci

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,12 +1,20 @@
 
+// module.exports = {
+//   "db": {
+//     "name": "db",
+//     "connector": "memory"
+//   },
+//   "MongoDB": {
+//     "url": "mongodb://localhost:27017",
+//     "name": "MongoDB",
+//     "connector": "mongodb"
+//   }
+// }
+
 module.exports = {
-  "db": {
-    "name": "db",
-    "connector": "memory"
-  },
   "MongoDB": {
-    "url": "mongodb://localhost:27017",
     "name": "MongoDB",
-    "connector": "mongodb"
+    "connector": "mongodb",
+    "url": process.env.MONGODB_URI
   }
 }
